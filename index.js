7d1d277cc20b97472e0575cffd9da341b97941ee
ERHolguin <rrodshedale@gmail.com>
2023-12-26 16:55:30 +0000

    Create Operation Conditions

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ function multiply(...args) {
 function divide(a, b) {
     return a / b;
 }
+
+let num1;
+let num2;
+let operator;
+
+function operate(operator, num1, num2) {
+    if(operator == '+') {
+        add(num1, num2);
+    } else if(operator == '-') {
+        subtract(num1, num2);
+    } else if(operator == '*') {
+        multiply(num1, num2);
+    } else if(operator == '/') {
+        divide(num1, num2);
+    } else {
+        return 'invalid input';
+    };
+};
+
+//console.log(operate(2, 3))
+
