bbc1050677c5219676811449db05b78df65759de
ERHolguin <rrodshedale@gmail.com>
2024-01-11 17:15:36 +0000

    Rename Calculator Button Classes According to their Functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let displayedInput = document.getElementById('displayWindowContent');
+/*let displayedInput = document.getElementById('displayWindowContent');
 let clearWindowButton = document.getElementById('AC')
-let calculatorButtons = document.querySelectorAll('button');
+let backspaceButton = document.getElementById('backspace')
+let calculatorButtons = document.querySelectorAll('button');*/
+
 
 function add(a, b) {
     return a + b
@@ -18,33 +20,50 @@ function divide(a, b) {
     return a / b;
 }
 
-let num1;
+/*let num1;
 let num2;
-let operator;
+let operator;*/
 
 function operate(num1, operator, num2) {
-    if(operator == '+') {
-        add(num1, num2);
-    } else if(operator == '-') {
-        subtract(num1, num2);
-    } else if(operator == '*') {
-        multiply(num1, num2);
-    } else if(operator == '/') {
-        divide(num1, num2);
+    if (operator == '+') {
+        return add(num1, num2);
+    } else if (operator == '-') {
+        return subtract(num1, num2);
+    } else if (operator == '*') {
+        return multiply(num1, num2);
+    } else if (operator == '/') {
+        return divide(num1, num2);
     } else {
         return 'invalid input';
     };
 };
 
-function deleteAll() {
-    displayedInput.innerText = '';  
-};
 
+//console.log(operate(2,'*',4));
 
+/*
 calculatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
         displayedInput.innerText += ' ' + button.innerText; 
     });
 });
 
+function deleteAll() {  
+    displayedInput.innerText = '';  
+    
+    };
+    
+
+
 clearWindowButton.addEventListener('click', deleteAll);
+//backspaceButton.addEventListener('click', deleteLastInput);
+
+/*function deleteLastInput() {  
+   return displayedInput.innerText.slice(0 -1);
+            
+    };
+
+/*let newArray = displayedInput.innerText.split('');
+newArray.splice(-1, 1);
+
+console.log(newArray)*/
