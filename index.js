d66e3a19d0db2571e7813831cfa652c14400d4a0
ERHolguin <rrodshedale@gmail.com>
2024-02-21 16:48:24 -0500

    Calculate User Inputs and Provide Results

--- index.js
+++ index.js
@@ -1,81 +1,69 @@
-let operationInput = document.getElementById('operationDisplayContent');
-let resultOutput = document.getElementById('resultDisplayContent');
-let clearWindowButton = document.getElementById('AC')
-let backspaceButton = document.getElementById('backspace')
+let operationInput = document.getElementById('operationDisplay');
+let resultOutput = document.getElementById('resultDisplay');
+let clearWindowButton = document.getElementById('AC') ;
+let backspaceButton = document.getElementById('backspace');
 let numberButtons = document.querySelectorAll('.number-button');
 let operationButtons = document.querySelectorAll('.operation-button');
-let equalsButton = document.getElementById('=')
+let equalsButton = document.getElementById('equals');
 
 function add(a, b) {
     return a + b
-}
+};
 
 function subtract(a, b) {
     return a - b;
-}
+};
 
 function multiply(...args) {
     return args.reduce((product, current) => product * current, 1);
-}
+};
 
 function divide(a, b) {
     return a / b;
-}
+};
 
-let num1 = '';
-let num2 = '';
-let operator = '';
-
-/*function operatorInput(e) {
-    let operator = e.target.innerText;
-    console.log(operator);
-    operationInput.innerText += operator;
-  };
-
-function numberInput(e) {
-    if(operator === '') {
-        let num1 = e.target.innerText
-        console.log(num1)
-        operationInput.innerText += num1
-    } else {
-        let num2 = e.target.innerText
-       operationInput.innerText += num2
-    }  
-};    */
+let num1;
+let num2;
+let operator;
 
 function userInput(e) {
     operationInput.innerText += e.target.innerText;
-    console.log(operationInput.innerText)
-}
+    console.log(operationInput.innerText);
+};
 
 
 function operate(num1, operator, num2) {
     if (operator == '+') {
         resultOutput.innerText = add(parseInt(num1), parseInt(num2));
         console.log(num1, operator, num2);
-        console.log(add(parseInt(num1), parseInt(num2)));
+        console.log(resultOutput.innerText);
     } else if (operator == '-') {
         resultOutput.innerText =  subtract(parseInt(num1), parseInt(num2));
         console.log(num1, operator, num2);
-        console.log(subtract(parseInt(num1), parseInt(num2)));
+        console.log(resultOutput.innerText);
     } else if (operator == 'x') {
         resultOutput.innerText =  multiply(parseInt(num1), parseInt(num2));
         console.log(num1, operator, num2);     
-        console.log(multiply(parseInt(num1), parseInt(num2)));
-    } else if (operator == '/') {
+        console.log(resultOutput.innerText);
+    } else if (operator == '÷') {
         resultOutput.innerText =  divide(parseInt(num1), parseInt(num2));
         console.log(num1, operator, num2);
-        console.log(divide(parseInt(num1), parseInt(num2)))
+        console.log(resultOutput.innerText);
     } else {
         resultOutput.innerText = 'invalid input';
-        console.log('Invalid Input')
+        console.log('Invalid Input');
     };
 };
 
+//console.log(operate(2,'+',9));
+
 function getResult() {
-    let splitResult = operationInput.innerText.split(',');
-    resultOutput.innerText = operate(splitResult);
+    let operation = operationInput.innerText;
+    let [num1, operator, num2] = operation.split(/(\+|-|\x|\÷)/);
+    resultOutput.innerText = operate(num1, operator, num2);
+    return resultOutput.innerText;
 };
+
 
 function deleteAll() {  
     operationInput.innerText = ''; 
@@ -90,11 +78,13 @@ numberButtons.forEach((button) => {
     button.addEventListener('click', userInput); 
 });
 
-equalsButton.addEventListener('click', getResult);
-
 operationButtons.forEach((button) => {
     button.addEventListener('click', userInput);    
 });
+
+equalsButton.addEventListener('click', getResult);
+
+
 
 clearWindowButton.addEventListener('click', deleteAll);
 
@@ -109,8 +99,4 @@ backspaceButton.addEventListener('click', deleteLastInput);
 //when user clicks on equals button:
     //- the click event should trigger the operate() function
     // - the click event should also update the display with the results
-
-//when user clicks on backspace button:
-    // - the last input should be deleted
-    // - the display should update, showing everything, minus the last input
 
